64410f22f7caed1f8cd9b43269eeb41fc23e91bf
Jhon Steven Saavedra <90219663+jsgsdev@users.noreply.github.com>
2021-09-07 10:03:25 -0500

    Delete $RECYCLE.BIN/S-1-5-21-3681636023-1471550773-1645402324-1001 directory

--- $RECYCLE.BIN/S-1-5-21-3681636023-1471550773-1645402324-1001/$RNDZITE/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '470ec73f834e95c9a57f7b6a87f06805'),
-    },
-  },
-});
--- $RECYCLE.BIN/S-1-5-21-3681636023-1471550773-1645402324-1001/$RNDZITE/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '905a8900-e983-4f90-96ed-2303af68435f'
-};
